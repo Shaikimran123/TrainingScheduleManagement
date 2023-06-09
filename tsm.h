c817a53deb4a859085f472c18ae2267d1db7511b
Imm_u___ <84969992+Shaikimran123@users.noreply.github.com>
2023-06-09 23:24:11 +0530

    Add files via upload

---
+++ tsm.h
@@ -0,0 +1,100 @@
+// #ifndef TSMA_H
+// #define TSMA_H
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+
+
+void MainFun();
+
+class admin
+{
+	public:
+		void createSchedule();
+		void updateSchedule();
+		void allotTrainer();
+		void generateReport();
+		void adminModules();
+};
+
+class faculty
+{
+          public:
+                  std::string loginId;
+                  void viewSchedule();
+                  void AcceptOrCancelSchedule();
+                  void facultyModules();
+		  void acceptASchedule();
+ 		  void cancelASchedule();
+};
+
+
+
+void fogetPassword(string);
+
+
+
+
+string sdateGreaterThanEdate(string,string);
+bool validBatchId(string);
+bool check(string);
+int countNoOfDays(string ,string);
+bool validateString(string str);
+int IntValid(int n);
+bool negativeValidate(int);
+bool validateMonth(string , string);
+bool facultyFound(string);
+bool uniqueBatchID(string);
+bool techValidateString(string);
+bool facultyFoundInTrainerallot(string);
+bool batchAllotmentFound(int,string,string);
+
+
+void viewAllSchedules(string);
+void viewAcceptedSchedules(string); 
+void viewCancelSchedules(string); 
+
+bool uniquetrainerAcceptedSchedule(int,string);
+bool canotAcceptSchedule(int,string);
+bool trainerAcceptedSchedules(int,string);
+
+bool uniquetrainerCancelSchedules(int,string);
+bool canotCancelSchedule(int,string); 
+bool checkcancelSchedules(int,string);
+
+
+
+
+
+
+void homepage();
+
+void adminwelcomepage();
+ 
+void adminLoginErrorMsg();
+void adminLoginAccessDeniedMsg();
+
+void facultywelcomepage();
+
+void facultyLoginErrorMsg(); 
+void facultyLoginAccessDeniedMsg();
+
+void adminsHomePage();
+void adminCreateScheduleMenu();
+void adminCreateScheduleSuccessMsg();
+void adminUpdateSchedule();
+void adminUpdateScheduleSuccessMsg();
+void adminalloTrainer();
+void adminallottrainerSuccessMsg(int,string,string);
+void admingenerateReport();
+void adminQuiteAppMsg();
+
+void facultyMenu(string);
+void facultyviewSchedule();
+void facultyQuitApp();
+
+
+
+
+// #endif
